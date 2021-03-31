c1fe9cf63383f576ebbdedc9464439c74242faab
Sergei3190 <sergkosya@mail.ru>
2021-03-31 20:41:10 +0300

    добавлен репозиторий в качестве прослойки между доступом к данным через ORM EF Core и предметной областью

---
+++ FarmerAndPartnersEF/Repository/IAsyncRepository.cs
@@ -0,0 +1,23 @@
+﻿using FarmerAndPartnersModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FarmerAndPartnersEF.Repository
+{
+    public interface IAsyncRepository
+    {
+        Task<int> AddCompanyAsync(Company company);
+        Task<int> DeleteCompanyAsync(Company company);
+        Task<int> UpdateCompanyAsync(Company company);
+        Task<int> AddUserAsync(User user);
+        Task<int> DeleteUserAsync(User user);
+        Task<int> UpdateUserAsync(User user);
+        Task<int> ExecuteQueryAsync(string sql, params object[] sqlParameters);
+        Task<List<Company>> GetCompaniesAsync();
+        List<ContractStatus> GetContractStatuses();
+        IEnumerable<Company> GetCompanies();
+        IEnumerable<User> GetUsers();
+        int GetCompaniesCount();
+        int GetUsersCount();
+    }
+}
