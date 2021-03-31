df724eaff0151974c1a093dbd3dc76de3d858a92
Sergei3190 <sergkosya@mail.ru>
2021-03-31 20:47:56 +0300

    добавлены классы для сериализации

---
+++ FarmerAndPartners/Helpers/SerializableObjects/CompanyCollection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace FarmerAndPartners.Helpers.SerializableObjects
+{
+    [XmlRoot("Companies")]
+    public class CompanyCollection
+    {
+        [XmlElement("Company")]
+        public List<SerializableCompany> Companies { get; set; }
+    }
+}
---
+++ FarmerAndPartners/Helpers/SerializableObjects/SerializableCompany.cs
@@ -0,0 +1,21 @@
+﻿namespace FarmerAndPartners.Helpers.SerializableObjects
+{
+    public class SerializableCompany
+    {
+        public SerializableCompany() { }
+        public SerializableCompany(int id, string name, int contractStatusId, string contractStatusDefinition, UsersCollection users)
+        {
+            Id = id;
+            Name = name;
+            ContractStatusId = contractStatusId;
+            ContractStatusDefinition = contractStatusDefinition;
+            Users = users;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ContractStatusId { get; set; }
+        public string ContractStatusDefinition { get; set; }
+        public UsersCollection Users { get; set; }
+    }
+}
