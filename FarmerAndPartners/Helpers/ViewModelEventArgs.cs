6202e0f9bf882a2a44029690483adc33392cba9a
Sergei3190 <sergkosya@mail.ru>
2021-03-31 20:48:43 +0300

    добавлен класс-событие

---
+++ FarmerAndPartners/Helpers/ViewModelEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FarmerAndPartners.Helpers
+{
+    public class ViewModelEventArgs : EventArgs
+    {
+        public readonly string message;
+        public readonly int oldViewModelId;
+
+        public ViewModelEventArgs(string message, int oldViewModelId)
+        {
+            this.message = message;
+            this.oldViewModelId = oldViewModelId;
+        }
+    }
+}
