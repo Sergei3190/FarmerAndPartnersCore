bd43c132b23d269126832be51cdcf7a1ab87d80d
Sergei3190 <sergkosya@mail.ru>
2021-03-31 20:50:12 +0300

    добавлены views

---
+++ FarmerAndPartners/Views/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using FarmerAndPartners.Helpers.NestedTypesFactories.BaseFactory;
+using FarmerAndPartners.ViewModels;
+using System.Windows;
+
+namespace FarmerAndPartners.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+      
+            tabControlRoot.DataContext = new MainWindowViewModel(new BaseNestedTypesFactory());
+        }
+    }
+}
